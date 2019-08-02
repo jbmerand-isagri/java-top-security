904cafceaa34b73bf72693b41f738f95a277238c
jbmerand <jbmerand@icloud.com>
2019-08-02 17:15:53 +0200

    securisation : preparedStatement pour creation d'un utilisateur

--- src/main/java/dev/services/UserService.java
+++ src/main/java/dev/services/UserService.java
@@ -1,22 +1,23 @@
 package dev.services;
 
+import java.util.List;
+
 import dev.domains.User;
 import dev.utils.DbUtils;
 
-import java.util.List;
-
 public class UserService {
 
-    private static final String INSERT_USER_SQL = "insert into user(firstname,lastname,login, password) values('%s','%s', '%s', '%s')";
-    private static final String SELECT_ALL_USERS_SQL = "select * from user";
+	private static final String INSERT_USER_SQL = "insert into user(firstname,lastname,login, password) values(?,?,?,?)";
+	private static final String SELECT_ALL_USERS_SQL = "select * from user";
 
-    private DbUtils dbUtils = new DbUtils();
+	private DbUtils dbUtils = new DbUtils();
 
-    public void save(User user) {
-        this.dbUtils.executeInsert(String.format(INSERT_USER_SQL, user.getFirstname(), user.getLastname(), user.getLogin(), user.getPassword()));
-    }
+	public void save(User user) {
+		this.dbUtils.executeInsert(INSERT_USER_SQL, user.getFirstname(), user.getLastname(), user.getLogin(),
+				user.getPassword());
+	}
 
-    public List<User> list() {
-        return this.dbUtils.executeSelect(SELECT_ALL_USERS_SQL, resultSet -> this.dbUtils.resultSetToUser(resultSet));
-    }
+	public List<User> list() {
+		return this.dbUtils.executeSelect(SELECT_ALL_USERS_SQL, resultSet -> this.dbUtils.resultSetToUser(resultSet));
+	}
 }
--- src/main/java/dev/utils/DbUtils.java
+++ src/main/java/dev/utils/DbUtils.java
@@ -1,56 +1,66 @@
 package dev.utils;
 
-import dev.domains.User;
-import dev.exceptions.AppException;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+import dev.domains.User;
+import dev.exceptions.AppException;
+
 public class DbUtils {
 
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:3307/top_securite_bdd?characterEncoding=UTF-8";
-    private static final String DATABASE_USER = "root";
-    private static final String DATABASE_PASS = "root";
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3307/top_securite_bdd?characterEncoding=UTF-8";
+	private static final String DATABASE_USER = "root";
+	private static final String DATABASE_PASS = "root";
 
-    public User resultSetToUser(ResultSet rs) {
-        try {
-            return new User(rs.getInt("id"), rs.getString("firstname"), rs.getString("lastname"), rs.getString("login"), rs.getString("password"), rs.getBoolean("is_admin"));
-        } catch (SQLException e) {
-            throw new AppException(e);
-        }
-    }
+	public User resultSetToUser(ResultSet rs) {
+		try {
+			return new User(rs.getInt("id"), rs.getString("firstname"), rs.getString("lastname"), rs.getString("login"),
+					rs.getString("password"), rs.getBoolean("is_admin"));
+		} catch (SQLException e) {
+			throw new AppException(e);
+		}
+	}
 
-    public <T> List<T> executeSelect(String sql, Function<ResultSet, T> fn) {
+	public <T> List<T> executeSelect(String sql, Function<ResultSet, T> fn) {
 
-        List<T> list = new ArrayList<>();
+		List<T> list = new ArrayList<>();
 
-        try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
-             PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet rs = statement.executeQuery()) {
+		try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
+				PreparedStatement ps = connection.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery()) {
 
-            while (rs.next()) {
-                list.add(fn.apply(rs));
-            }
+			while (rs.next()) {
+				list.add(fn.apply(rs));
+			}
 
-        } catch (SQLException e) {
-            throw new AppException(e);
-        }
+		} catch (SQLException e) {
+			throw new AppException(e);
+		}
 
-        return list;
-    }
+		return list;
+	}
 
-    public void executeInsert(String sql) {
-        try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
-             Statement ps = connection.createStatement()
-        ) {
+	public void executeInsert(String sql, String userFirstName, String userLastname, String userLogin,
+			String userPassword) {
+		try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
+				PreparedStatement ps = connection.prepareStatement(sql)) {
+			ps.setString(1, userFirstName);
+			ps.setString(2, userLastname);
+			ps.setString(3, userLogin);
+			ps.setString(4, userPassword);
 
-            ps.executeUpdate(sql);
+			ps.executeUpdate();
+			ps.close();
 
-        } catch (SQLException e) {
-            throw new AppException(e);
-        }
+		} catch (SQLException e) {
+			throw new AppException(e);
+		}
 
-    }
+	}
 }
